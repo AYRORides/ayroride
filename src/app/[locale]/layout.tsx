4d382054316bb12795de56d6e69ff49c2d970ee0
Swotboysandy <swotboysandy@gmail.com>
2026-01-14 20:14:56 +0530

    Fix params await and move css

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -58,11 +58,13 @@ import Script from "next/script";
 
 export default async function RootLayout({
   children,
-  params: { locale }
+  params
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }) {
+  const { locale } = await params;
+
   // Providing all messages to the client
   // side is the easiest way to get started
   const messages = await getMessages();
